f383e02c1abb89c9efe2cd4fff555d9b06ff1849
Henvealf <1103693087@qq.com>
2016-05-15 15:28:54 +0800

    获取了一下各种控件,单选按钮加了个监听器.

--- app/src/main/java/com/lanbitou/activities/AddBillActivity.java
+++ app/src/main/java/com/lanbitou/activities/AddBillActivity.java
@@ -4,9 +4,14 @@ package com.lanbitou.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
 
 import com.lanbitou.R;
+import com.lanbitou.entities.Bill;
 
 /**
  * 添加
@@ -15,6 +20,15 @@ import com.lanbitou.R;
 public class AddBillActivity extends Activity {
 
     ImageButton backImgBtn;
+    EditText typeEt;        //消费类型
+    EditText moneyEt;       //多少钱
+    RadioGroup inOutRg;         //收入还是支出
+    DatePicker datePicker;      //日期选择器
+    Button addBtn;              //添加按钮
+
+    Bill bill;
+
+    boolean isIn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +36,6 @@ public class AddBillActivity extends Activity {
         setContentView(R.layout.activity_add_bill);
 
         backImgBtn = (ImageButton) findViewById(R.id.add_bill_back_btn);
-
         backImgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,5 +43,49 @@ public class AddBillActivity extends Activity {
             }
         });
 
+        typeEt = (EditText) findViewById(R.id.add_bill_type_et);
+        moneyEt = (EditText) findViewById(R.id.add_bill_money_et);
+
+        inOutRg = (RadioGroup) findViewById(R.id.add_bill_inout_rg);
+        findViewById(R.id.add_bill_rb_in);
+        findViewById(R.id.add_bill_rb_out);
+
+        //inOutRg.setOnCheckedChangeListener();
+
+        datePicker = (DatePicker) findViewById(R.id.add_bill_date);
+
+        addBtn = (Button) findViewById(R.id.add_bill_btn);
+
+        bill = new Bill();
     }
+
+    private class InOutCheckedChangeListener implements RadioGroup.OnCheckedChangeListener{
+
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int i) {
+            switch (i){
+                case R.id.add_bill_rb_in:
+                    isIn = true;
+                    break;
+                case R.id.add_bill_rb_out:
+                    isIn = false;
+                    break;
+            }
+        }
+    }
+
+
+    private class AddBtnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+            String type = typeEt.getText().toString();
+
+            bill.setUid(1);
+            bill.setFolder("日常");
+            //bill.setType();
+        }
+    }
+
+
 }
